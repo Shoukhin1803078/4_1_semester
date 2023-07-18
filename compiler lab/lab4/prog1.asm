3bf5f27de82b1c46268118e860ea1786c099ecb7
Md Al Amin Tokder Shoukhin <62458402+Shoukhin1803078@users.noreply.github.com>
2023-07-18 10:34:33 +0600

    prog1.asm

--- compiler lab/lab4/prog1.asm
+++ compiler lab/lab4/prog1.asm
@@ -1,1 +1,36 @@
+.686
+.model flat, c
+include C:\masm32\include\msvcrt.inc
+includelib C:\masm32\lib\msvcrt.lib
+
+.stack 100h
+printf PROTO arg1:Ptr Byte, printlist:VARARG
+scanf PROTO arg2:Ptr Byte, inputlist:VARARG
+
+.data
+input_integer_format byte "%d",0
+output_integer_msg_format byte "%d", 0Ah, 0
+number1 sdword ?
+number2 sdword ?
+
+.code
+main proc
+
+    ;scanf("%d", &number);
+    ;duiti number input niye add kore result dekhabo
+
+    INVOKE scanf, ADDR input_integer_format, ADDR number1
+    INVOKE scanf, ADDR input_integer_format, ADDR number2
+
+    MOV EAX,number1
+    MOV EBX,number2
+    ADD EAX,EBX
+
+    ;printf("%d\n", number);
+    INVOKE printf, ADDR output_integer_msg_format, EAX
+
+
+    ret
+main endp
+end
 
